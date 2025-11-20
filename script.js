3f82673286de6816b07d982a0f25a05ba01b6888
Farhaanriez <farhan.rizky.alkarim5@gmail.com>
2025-11-20 18:53:53 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,18 @@
+const toggleBtn = document.getElementById("toggleFeatures");
+const allCards = document.querySelectorAll(".feature-card");
+
+let expanded = false;
+
+toggleBtn.addEventListener("click", () => {
+    expanded = !expanded;
+
+    allCards.forEach((card, index) => {
+        if (index >= 3) {
+            card.classList.toggle("hidden", !expanded);
+        }
+    });
+
+    toggleBtn.innerHTML = expanded 
+        ? `Show Less <img src="assets/Icon2.svg"/>`
+        : `Show More <img src="assets/Icon2.svg"/>`;
+});
